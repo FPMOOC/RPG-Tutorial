a60d2bea157991675458d2a429e0ab9475f603eb
Nathan <nkmacadam@gmail.com>
2020-02-25 15:58:33 -0500

    Added in dialogue functionality

--- Assets/CharacterMotor.cs
+++ Assets/CharacterMotor.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class CharacterMotor : MonoBehaviour
 {
+    public float moveSpeed;
+    public DialogueRunner dialogueRunner;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,30 @@ public class CharacterMotor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 input = Vector2.zero;
+
+        if (Input.GetKeyDown(KeyCode.W))
+            input.y = 1;
+        if (Input.GetKeyDown(KeyCode.A))
+            input.x = -1;
+        if (Input.GetKeyDown(KeyCode.S))
+            input.y = -1;
+        if (Input.GetKeyDown(KeyCode.D))
+            input.x = 1;
+
+        transform.Translate(input * moveSpeed);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            var npc = GetComponent<NPC>();
+
+            if (npc != null)
+            {
+                dialogueRunner.ShowDialogue(npc);
+            }
+        }
     }
 }
---
+++ Assets/DialogueRunner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueRunner : MonoBehaviour
+{
+    [SerializeField] private Image image;
+    [SerializeField] private Text nameText;
+    [SerializeField] private Text dialogueText;
+    [SerializeField] private GameObject contentParent;
+
+    private bool isShowing = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isShowing)
+        {
+            if (Input.anyKeyDown)
+            {
+                contentParent.SetActive(false);
+                isShowing = false;
+            }
+        }
+    }
+
+    public void ShowDialogue(NPC npc)
+    {
+        isShowing = true;
+        contentParent.SetActive(true);
+
+        image.sprite = npc.NPCImage;
+        nameText.text = npc.name;
+        dialogueText.text = npc.dialogue;
+    }
+}
